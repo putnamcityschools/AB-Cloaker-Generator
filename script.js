985a15544e7ff3e539f587606b4cdf8ec484a7ff
putnamcityschools <h8156072@gmail.com>
2025-01-21 13:28:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function opengame() {
 	var win = window.open()   
-	var url = "https://putnamcityschools.github.io/mathpunch.github.io/" 
+	var url = "https://putnamcityschools.github.io/mathpunchgithub.io/" 
 	var iframe = win.document.createElement('iframe')
 	iframe.style.width = "100%";
 	iframe.style.height = "100%";
