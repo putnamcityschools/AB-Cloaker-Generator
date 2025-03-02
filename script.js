fec962c59ec6a6e0acd6ce95956357b7a05f8854
putnamcityschools <h8156072@gmail.com>
2025-03-01 23:57:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function opengame() {
 	var win = window.open()   
-	var url = "https://mathpunchproxy.github.io" 
+	var url = "https://vidlink.pro/movie/912649" 
 	var iframe = win.document.createElement('iframe')
 	iframe.style.width = "100%";
 	iframe.style.height = "100%";
